adb646febb3c8f6a843231e6c1899fa234ac8413
Sudhakardas <das.sudhakar@braintoy.ai>
2024-01-23 17:38:38 +0600

    created new html file and a addtion function in the master branch

--- nestedcv.py
+++ nestedcv.py
@@ -3,3 +3,6 @@
 print("Learning mearging , this is from the sudha branch")
 print('new line in sudha')
 
+def add(a, b):
+    c = a+b
+    print("Now in master branch i am creating a function for adding two number")
